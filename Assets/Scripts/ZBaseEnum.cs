8d7f9540e5efbb289ef04cfb6fb58ed862af0fd3
minhdt <minhdt@zitga.com.vn>
2020-10-16 00:45:36 +0700

    + add feature

---
+++ Assets/Scripts/ZBaseEnum.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZBaseEnum
+{
+    public enum Status
+    {
+        installed = 1,
+        none = 2,
+        updated = 3
+    }
+
+    public enum Source
+    {
+        registry,
+        builtin,
+        git,
+        embedded
+    }
+}
---
+++ Assets/Scripts/ZBasePackageIdConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZBasePackageIdConfig
+{
+    public static readonly string namePackageManager = "com.zitga.packagetest";
+    public static readonly Dictionary<string, string> listPackages = new Dictionary<string, string>() {
+        { "com.zitga.packagetest", "Package Manager" },
+        { "com.cysharp.unitask", "Unitask" },
+        { "com.unity.2d.animation", "Animation" },
+    };
+}
