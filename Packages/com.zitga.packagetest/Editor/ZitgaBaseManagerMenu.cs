91fefc2d683a302f87979b3b54eba007e27f5443
minhdt <minhdt@zitga.com.vn>
2020-10-16 09:40:11 +0700

    + upgrade tool

--- Packages/com.zitga.packagetest/Editor/ZitgaBaseManagerMenu.cs
+++ Packages/com.zitga.packagetest/Editor/ZitgaBaseManagerMenu.cs
@@ -6,7 +6,7 @@ public class ZitgaBaseManagerMenu
     [MenuItem("ZitgaBase/Tool Manager", false, 0)]
     public static void ToolManager()
     {
-        Debug.Log("Tool Manager");
+        //Debug.Log("Tool Manager");
         ZBaseDependenciesManager.ShowZBaseDependenciesManager();
     }
 }
